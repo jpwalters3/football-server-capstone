d9df38630bed255b94dac8cc7aacaaf6ecb3013e
Mabel Quek <mxquek1@gmail.com>
2022-05-10 16:13:37 -0500

    VS Project w/ gitignore

---
+++ FootballServerCapstone/FootballServerCapstone.Core/Interfaces/DAL/IPerformanceRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballServerCapstone.Core.Entities;
+
+namespace FootballServerCapstone.Core.Interfaces.DAL
+{
+    public interface IPerformanceRepository
+    {
+        public Response<Performance> Insert(Performance performance);
+        public Response Delete(int matchId, int playerId);
+        public Response Update(Loan performance);
+
+        public Response<Performance> GetById(int matchId, int playerId);
+        public Response<List<Performance>> GetAll();
+        public Response<List<Performance>> GetByMatch(int matchId);
+        public Response<List<Performance>> GetByPlayer(int playerId);
+        public Response<List<Performance>> GetByPosition(int positionId);
+    }
+}
---
+++ FootballServerCapstone/FootballServerCapstone.Core/Interfaces/DAL/IPlayerRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballServerCapstone.Core.Entities;
+
+
+namespace FootballServerCapstone.Core.Interfaces.DAL
+{
+    public interface IPlayerRepository
+    {
+        public Response<Player> Insert(Player player);
+        public Response Delete(int playerId);
+        public Response Update(Player player);
+
+        public Response<Player> GetById(int playerId);
+        public Response<List<Player>> GetAll();
+        public Response<List<Player>> GetByClub(int clubId);
+        public Response<List<History>> GetHistory(int playerId);
+        public Response<List<Loan>> GetLoans(int playerId);
+    }
+}
