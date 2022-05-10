97a8dabd0a3561a99d9214db9e9c7548d2332a9e
Kevin-Kyle Nwosu <KKNwosu@dev-10.com>
2022-05-10 14:02:58 -0500

    Made WebApi Project, Club Controller and Club Models

---
+++ FootballServerCapstone/FootballServerCapstone.API/Models/ViewClubModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballServerCapstone.API.Models
+{
+    public class ViewClubModel
+    {
+        public int ClubId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Founding Date is required")]
+        public DateTime FoundingDate { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City cannot be longer than 50 characters.")]
+        public string City { get; set; }
+    }
+}
