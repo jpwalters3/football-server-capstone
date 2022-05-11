f96aeeefedcc639e77d28d5e0c30b7f05108b0e0
Mabel Quek <mxquek1@gmail.com>
2022-05-11 00:42:33 -0500

    Created base DAL, ClubRepoTests, & complete ClubRepo. Updated Response entity

---
+++ FootballServerCapstone/FootballServerCapstone.DAL/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using FootballServerCapstone.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace FootballServerCapstone.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Club> Club { get; set; }
+        public DbSet<History> History { get; set; }
+        public DbSet<Loan> Loan { get; set; }
+        public DbSet<Match> Match { get; set; }
+        public DbSet<Performance> Performance { get; set; }
+        public DbSet<Player> Player { get; set; }
+        public DbSet<Position> Position { get; set; }
+        public DbSet<Season> Season { get; set; }
+
+        public AppDbContext() : base()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(message => Debug.WriteLine(message), LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Performance>()
+                .HasKey(p => new { p.MatchId, p.PlayerId });
+        }
+    }
+}
---
+++ FootballServerCapstone/FootballServerCapstone.DAL/ConfigProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FootballServerCapstone.DAL
+{
+    public class ConfigProvider
+    {
+        public IConfigurationRoot Config { get; private set; }
+        public ConfigProvider()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets<AppDbContext>();
+            Config = builder.Build();
+        }
+    }
+}
