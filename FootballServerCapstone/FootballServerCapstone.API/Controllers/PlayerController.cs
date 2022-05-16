0454097abf1f118154fdb3c453518954e3df03ca
Mabel Quek <mxquek1@gmail.com>
2022-05-16 16:34:33 -0500

    Updated PlayerRepo & Controller GetHistory() NotFound handling

--- FootballServerCapstone/FootballServerCapstone.API/Controllers/PlayerController.cs
+++ FootballServerCapstone/FootballServerCapstone.API/Controllers/PlayerController.cs
@@ -105,7 +105,7 @@ namespace FootballServerCapstone.API.Controllers
             }
             else
             {
-                if (history.Data == null)
+                if (history.Data.Count == 0)
                 {
                     return NotFound(history.Message);
                 }
