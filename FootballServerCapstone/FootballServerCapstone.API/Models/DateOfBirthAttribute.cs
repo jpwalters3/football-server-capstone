d242f10714d8c63db03c746bffcfdf3e25131929
Mabel Quek <mxquek1@gmail.com>
2022-05-18 21:17:30 -0500

    Updated DateOfBirthAttributes

--- FootballServerCapstone/FootballServerCapstone.API/Models/DateOfBirthAttribute.cs
+++ FootballServerCapstone/FootballServerCapstone.API/Models/DateOfBirthAttribute.cs
@@ -5,26 +5,27 @@ namespace FootballServerCapstone.API.Models
     public class DateOfBirthAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-            {
-                if (value is DateTime)
-                {
-                    DateTime date = Convert.ToDateTime(value);
+        {
+            DateTime min = DateTime.Now.AddYears(-40).Date; //1982-05-10
+            DateTime max = DateTime.Now.AddYears(-20).Date; //2002-05-10
 
-                    if (date <= DateTime.Parse("1982-05-10") && date >= DateTime.Parse("2002-05-10"))
-                    {
-                        return new ValidationResult("Date must be between 5/10/82 and 5/10/02");
-                    }
-                    else
-                    {
-                        return ValidationResult.Success;
-                    }
+            if (value is DateTime)
+            {
+                DateTime date = Convert.ToDateTime(value);
+
+                if (date < min || date > max)
+                {
+                    return new ValidationResult($"Date must be between {min:MM/dd/yyyy} and {max:MM/dd/yyyy}");
                 }
                 else
                 {
-                    return new ValidationResult("This attribute only works with DateTime objects");
+                    return ValidationResult.Success;
                 }
-
-            
+            }
+            else
+            {
+                return new ValidationResult("This attribute only works with DateTime objects");
+            }
         }
     }
 }
